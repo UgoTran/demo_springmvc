952d332f665db3b8a209f8b3860f402f67f66d52
uralugo <bsk.denimit@gmail.com>
2025-11-15 01:36:44 +1100

    tạo api hứng object đẩy từ FE ajax đến BE

--- src/main/java/com/t3h/demo/ThymeleafDemoController.java
+++ src/main/java/com/t3h/demo/ThymeleafDemoController.java
@@ -1,11 +1,17 @@
 package com.t3h.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
@@ -16,11 +22,11 @@ import java.util.List;
 public class ThymeleafDemoController {
 
     static List<Person> persons = List.of(
-            new Person("A12",15, LocalDate.of(1999, 1, 1), "TESTA", 100.5, "https://shopping.viavilab.com/assets/images/products/thumbs/32_150x150.jpg"),
-            new Person("A13",16, LocalDate.of(1997, 5, 4), "jounA", 101.6, "https://shopping.viavilab.com/assets/images/products/thumbs/09012020043156_59802_150x150.jpg"),
-            new Person("A14",19, LocalDate.of(1998, 11, 7), "basdA", 103.1, "https://shopping.viavilab.com/assets/images/products/thumbs/77_150x150.jpg"),
-            new Person("A15",21, LocalDate.of(1996, 6, 2), "sadas", 104.4, null),
-            new Person("A16",31, LocalDate.of(1994, 6, 2), "humanet", 1.4, null)
+            new Person("A12", 15, LocalDate.of(1999, 1, 1), "TESTA", 100.5, "https://shopping.viavilab.com/assets/images/products/thumbs/32_150x150.jpg"),
+            new Person("A13", 16, LocalDate.of(1997, 5, 4), "jounA", 101.6, "https://shopping.viavilab.com/assets/images/products/thumbs/09012020043156_59802_150x150.jpg"),
+            new Person("A14", 19, LocalDate.of(1998, 11, 7), "basdA", 103.1, "https://shopping.viavilab.com/assets/images/products/thumbs/77_150x150.jpg"),
+            new Person("A15", 21, LocalDate.of(1996, 6, 2), "sadas", 104.4, null),
+            new Person("A16", 31, LocalDate.of(1994, 6, 2), "humanet", 1.4, null)
     );
 
     @GetMapping("/persons")
@@ -53,9 +59,29 @@ public class ThymeleafDemoController {
         pDetailsView.addObject("p", p);
         pDetailsView.addObject("idInput", id);
         pDetailsView.setViewName("personDetails");
-        return  pDetailsView;
+        return pDetailsView;
     }
 
+    @GetMapping("/preson/create")
+    public ModelAndView newPerson() {
+        return new ModelAndView("newPerson");
+    }
+
+    /**
+     * FE đẩy JSON person
+     * BE nhận vào object person
+     * thêm ID mới
+     * response person chuyển về JSON
+     */
+    @PostMapping("/api/person")
+    public ResponseEntity<Person> newPerson(@RequestBody Person person) {
+
+        person.setId("B11");
+        log.info("newPerson person={}", person);
+
+        //trả person tự động chuyển về JSON(mặc định)
+        return new ResponseEntity<>(person, HttpStatus.CREATED);
+    }
 
 
 }
